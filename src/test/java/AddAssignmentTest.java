aa81c4d30ad08fb171b46542e251f8cff64d74f6
Claudiu Codrea <clau.codrea@yahoo.com>
2019-04-16 19:39:26 +0300

    WBT More Assignment tests

--- src/test/java/AddAssignmentTest.java
+++ src/test/java/AddAssignmentTest.java
@@ -1,5 +1,6 @@
 import Exceptions.ValidatorException;
 import Exceptions.ServiceException;
+import Domain.TemaLab;
 import Repository.XMLFileRepository.TemaLabXMLRepo;
 import Service.XMLFileService.TemaLabXMLService;
 import Validator.TemaLabValidator;
@@ -93,5 +94,84 @@ public class AddAssignmentTest {
         String[] params = {null, "a", "2", "2"};
 
         tmsrv.add(params);
+    }
+
+    @Test(expected = ValidatorException.class)
+    public void termenLT1() throws ValidatorException {
+        //given
+
+        TemaLabValidator vt = new TemaLabValidator();
+        TemaLabXMLRepo tmrepo = new TemaLabXMLRepo(vt, "TemaLaboratorXML.xml");
+        TemaLabXMLService tmsrv = new TemaLabXMLService(tmrepo);
+        //given
+        String[] params = {"1", "a", "0", "2"};
+
+        tmsrv.add(params);
+
+    }
+
+    @Test(expected = ValidatorException.class)
+    public void termenGT14() throws ValidatorException {
+        //given
+
+        TemaLabValidator vt = new TemaLabValidator();
+        TemaLabXMLRepo tmrepo = new TemaLabXMLRepo(vt, "TemaLaboratorXML.xml");
+        TemaLabXMLService tmsrv = new TemaLabXMLService(tmrepo);
+        //given
+        String[] params = {"1", "a", "15", "2"};
+
+        tmsrv.add(params);
+    }
+
+    @Test(expected = ValidatorException.class)
+    public void predareLT1() throws ValidatorException {
+        //given
+
+        TemaLabValidator vt = new TemaLabValidator();
+        TemaLabXMLRepo tmrepo = new TemaLabXMLRepo(vt, "TemaLaboratorXML.xml");
+        TemaLabXMLService tmsrv = new TemaLabXMLService(tmrepo);
+        //given
+        String[] params = {"1", "a", "2", "0"};
+
+        tmsrv.add(params);
+
+    }
+
+    @Test(expected = ValidatorException.class)
+    public void predareGT14() throws ValidatorException {
+        //given
+
+        TemaLabValidator vt = new TemaLabValidator();
+        TemaLabXMLRepo tmrepo = new TemaLabXMLRepo(vt, "TemaLaboratorXML.xml");
+        TemaLabXMLService tmsrv = new TemaLabXMLService(tmrepo);
+        //given
+        String[] params = {"1", "a", "2", "15"};
+
+        tmsrv.add(params);
+    }
+
+    @Test(expected = ServiceException.class)
+    public void repoDuplicateId() throws ValidatorException {
+        //given
+
+        TemaLabValidator vt = new TemaLabValidator();
+        TemaLabXMLRepo tmrepo = new TemaLabXMLRepo(vt, "TemaLaboratorXML.xml");
+        TemaLabXMLService tmsrv = new TemaLabXMLService(tmrepo);
+        //given
+        String[] params = {"1", "a", "2", "2"};
+
+        tmsrv.add(params);
+
+        tmrepo.save(new TemaLab(Integer.parseInt(params[0]),params[1],Integer.parseInt(params[2]),Integer.parseInt(params[3])));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void nullEntity() throws ValidatorException {
+        //given
+
+        TemaLabValidator vt = new TemaLabValidator();
+        TemaLabXMLRepo tmrepo = new TemaLabXMLRepo(vt, "TemaLaboratorXML.xml");
+
+        tmrepo.save(null);
     }
 }
